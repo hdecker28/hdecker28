6a95238929a6c925b30ffcb65e7a08a865ac88b8
Hadashah Decker <115926669+hdecker28@users.noreply.github.com>
2022-10-16 18:54:27 +0800

    Add files via upload

---
+++ section1-week3-hadashah-decker.py.py
@@ -0,0 +1,22 @@
+######################################################
+# Hadashah Decker
+# hadashahdecker@gmail.com
+# Intro to Python, Section 1
+# Version 1.0
+# 09/14/2022
+# section1-week3-hadashah-decker.py
+# This program will print five of my favorite quotes.
+######################################################
+
+##PSEUDOCODE
+##1. Make a list of my favorite quotes
+##2. Use print statements to display the quotes
+##3. Check for mistakes
+
+print("""" Gravity doesn't work." - Darius Fairhead""")
+print(""""I was on YouTube Shorts for 3 seconds and then an hour went by." - Matthew Burgos""")
+print(""""Time to yeet that ice." - David Sell""")
+print(""""How am I supposed to romanticize my life if there's no pimple aesthetic on Pinterest." - Teshuah Decker""")
+print(""""Why do singers keep singing after they get old...like go work at Taco Bell." - Deo Natividad""")
+
+#These are some quotes my friends said.
